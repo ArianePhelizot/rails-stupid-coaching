5eeed26615a07d16181fadbfbb8300987d7300bb
Ariane PHELIZOT <ariane.phelizot@gmail.com>
2017-05-03 15:48:33 +0200

    stupid coaching done

--- app/controllers/coaching_controller.rb
+++ app/controllers/coaching_controller.rb
@@ -1,8 +1,9 @@
+require "stupid_coach.rb"
+
 class CoachingController < ApplicationController
   def answer
     @question =  params[:question]
-
-
+    @answer = coach_answer_enhanced(@question)
   end
 
   def question
